6c9e58c749e0076bedaa3c6e52c3e4a9aaf1f963
Rihards <112946496+rihards-freimanis@users.noreply.github.com>
2026-09-01 18:35:38 +0000

    [skip ci] release 1.0.241

--- Formula/erun.rb
+++ Formula/erun.rb
@@ -1,7 +1,7 @@
 class Erun < Formula
   desc "Multi-tenant multi-environment deployment and management tool"
   homepage "https://github.com/sophium/erun"
-  url "https://github.com/sophium/erun/archive/refs/tags/v1.0.240.tar.gz"
+  url "https://github.com/sophium/erun/archive/refs/tags/v1.0.241.tar.gz"
   sha256 "8560d34d2e5be6f22e41298fca6ebd8d9f95cacbd58c9c2d1e5cff53da9f4ee1"
   license "MIT"
 
