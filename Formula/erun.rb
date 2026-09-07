5449efaf62d1c6c8ecc7416e3143a086ea2c3ada
Rihards <112946496+rihards-freimanis@users.noreply.github.com>
2026-09-07 05:53:05 +0000

    [skip ci] sync package metadata 1.0.260

--- Formula/erun.rb
+++ Formula/erun.rb
@@ -2,7 +2,7 @@ class Erun < Formula
   desc "Multi-tenant multi-environment deployment and management tool"
   homepage "https://github.com/sophium/erun"
   url "https://github.com/sophium/erun/archive/refs/tags/v1.0.260.tar.gz"
-  sha256 "8aaad3b03439c99cb3760b84901d6639b4e2e9a84cc2776d176917a7f9ac5fb4"
+  sha256 "dbf58b718b91291a3dbf6b575a946790a3e22c18f3a0717a94a471a26813cff0"
   license "MIT"
 
   depends_on "go" => :build
